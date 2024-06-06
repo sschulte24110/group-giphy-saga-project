bb763de28cea972b0fd8273a03d6e5d9b49e6491
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-06 17:14:19 -0500

    Add router to get gifs from Giphy using API

---
+++ server/routes/giphy.router.js
@@ -0,0 +1,22 @@
+const pool = require('../modules/pool')
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+ router.get('/:search', (req, res) => {
+  const GIPHY_API_KEY = process.env.GIPHY_API_KEY2;
+  console.log(GIPHY_API_KEY);
+  const searchQuery = req.params.search;
+  console.log(searchQuery);
+ // https://api.giphy.com/v1/gifs/search?api_key=C4SWFxNbjLRDKEXZjAECdm4zFmYtMAFq&q=dogsd
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${searchQuery}`)
+  .then((response) => {
+    console.log(response.data.data);
+    res.send(response.data.data);
+  }).catch((error) => {
+    // console.error(error)
+    res.sendStatus(500);
+  })
+ })
+
+ module.exports = router;
