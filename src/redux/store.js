bbb2e5b9768bdd0f42dda70f9fd211d5e36e7dc7
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-06 17:12:55 -0500

    Add saga and reducer to get from axios

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,20 @@ import { takeLeading, put } from 'redux-saga/effects';
 import logger from 'redux-logger';
 import axios from 'axios';
 
-function* rootSaga() {}
+function* rootSaga() {
+  yield takeLeading('FETCH_SEARCH', fetchSearch);
+}
 
 const sagaMiddleware = createSagaMiddleware();
+
+function* fetchSearch(action) {
+  try{
+    const response = yield axios.get(`api/search/${action.payload}`);
+    yield put({ type: 'SET_GIFS', payload: response.data})
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const trending = (state = [], action) => {
   return state;
@@ -17,6 +28,9 @@ const favorites = (state = [], action) => {
 };
 
 const search = (state = [], action) => {
+  if (action.type === 'SET_GIFS') {
+    return action.payload;
+  }
   return state;
 };
 
