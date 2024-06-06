9a5667797f2ab14510026faa0c6d25d1975ff312
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-06 17:13:50 -0500

    Add giphyRouter to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
+const giphyRouter = require('./routes/giphy.router')
 const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -12,6 +14,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
+app.use('/api/search', giphyRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
