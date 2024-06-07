ffb6fd34802f90eac56cf709c43b53c05202b541
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-06 21:35:39 -0500

    Add addFavoriteSaga function and ADD_FAVORITE to favorite reducer. Need to check with Amber's work to make sure everything matches up.

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,13 @@ import logger from 'redux-logger';
 import axios from 'axios';
 
 function* rootSaga() {
-  yield takeLeading('FETCH_SEARCH', fetchSearch);
+  yield takeLeading('FETCH_GIFS', fetchGifsSaga);
+  yield takeLeading('ADD_FAVORITE', addFavoriteSaga);
 }
 
 const sagaMiddleware = createSagaMiddleware();
 
-function* fetchSearch(action) {
+function* fetchGifsSaga(action) {
   try{
     const response = yield axios.get(`api/search/${action.payload}`);
     yield put({ type: 'SET_GIFS', payload: response.data})
@@ -19,11 +20,26 @@ function* fetchSearch(action) {
   }
 }
 
+// Need to check with Amber's work to see if this will be correct.
+function* addFavoriteSaga(action) {
+  try {
+    yield axios.post('api/favorites', { url: action.payload});
+    yield put({ type: 'FETCH_FAVORITES' });
+  } catch (error) {
+    alert(`Error adding Favorite`);
+    console.error(error);
+  }
+}
+
 const trending = (state = [], action) => {
   return state;
 };
 
+// Did not add FETCH_FAVORITES since I did not know what Amber had completed.
 const favorites = (state = [], action) => {
+  if (action.payload === 'ADD_FAVORITE') {
+    return [...state, action.payload]
+  }
   return state;
 };
 
