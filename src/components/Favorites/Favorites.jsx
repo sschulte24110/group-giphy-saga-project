a2e1b1c2c93bbc9420c22a3c37d8bc0a2a9a306b
Amber Schaack <aschaack9@gmail.com>
2024-06-07 10:31:26 -0500

    favs moved to items to add category

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import Box from '@mui/material/Box';
+import FavoritesItem from '../FavoritesItem/FavoritesItem';
 
 export default function Favorites() {
     const dispatch = useDispatch();
     const favorites = useSelector(store => store.favorites);
-    const categories = useSelector(store => store.categories);
-    const [selectCategory, setSelectCategory] = useState({});
     console.log(favorites);
 
     useEffect(() => {
@@ -23,42 +12,11 @@ export default function Favorites() {
         dispatch({ type: 'FETCH_CATEGORIES' })
     }, []);
 
-    const handleChange = (event) => {
-        console.log('change from dropdown', event.target.value);
-        setSelectCategory(event.target.value);
-    }
-
     return (
         <>
         <h2>Favorite GIFs</h2>
         {favorites.map(favorite => (
-            <Card sx={{ width: 300, height: 350 }} key={favorite.id} id="card">
-                <CardMedia
-                    sx={{ height: 200 }}
-                    image={favorite.gif_url} />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {favorite.gif_name}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Remove</Button>
-                    <div id="dropdown">
-                    <InputLabel id="demo-simple-select-label">Category</InputLabel>
-                    <Select labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        placeholder='Category'
-                        value={selectCategory}
-                        label="Category"
-                        onChange={handleChange}
-                        >
-                        {categories.map(category => (
-                            <MenuItem key={category.id} value={category}>{category.name}</MenuItem>
-                        ))}
-                    </Select>
-                    </div>
-                </CardActions>
-            </Card>
+            <FavoritesItem key={favorite.id} favorite={favorite} />
         ))}
         </>
     );
