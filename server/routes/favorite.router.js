d714de896d2042b8065e6147d5d1cb94fb291505
Scott Schulte <scott.r.schulte@gmail.com>
2024-06-06 22:04:48 -0500

    Add GET and POST to favorite.router.

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,12 +5,28 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  const queryText = 'SELECT * FROM "favorites";';
+  pool.query(queryText).then((result) => {
+    res.send(result.rows);
+  }).catch((error) => {
+    console.log(`Error GETting`, error);
+    res.sendStatus(500);
+  })
 });
 
 // add a new favorite
+// Not tested in Postman since we didn't have any favorites
 router.post('/', (req, res) => {
-  res.sendStatus(201);
+  const newFavorite = req.body;
+  const queryText = `INSERT into "favorites" ("category_id", "gif_name", "gif_url") VALUES ($1, $2, $3);`;
+  const queryValues = [newFavorite.category_id, newFavorite.gif_name, newFavorite.gif_url];
+  pool.query(queryText, queryValues)
+  .then((result) => {
+    res.sendStatus(201);
+  }).catch((error) => {
+    console.log(`Error POSTing new favorite`, error);
+    res.sendStatus(500);
+  })
 });
 
 // update a favorite's associated category
